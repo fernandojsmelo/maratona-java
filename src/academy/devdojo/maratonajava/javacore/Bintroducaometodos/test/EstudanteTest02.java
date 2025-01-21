b6b8fec7f566865f970815a1c6a4ca96238e6f1a
fernandojsmelo <fernandojsmelo@gmail.com>
2025-01-21 09:54:13 -0300

    51 - Orientação Objetos - Métodos pt 08 - Referência this

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante1 = new Estudante();
+        Estudante estudante2 = new Estudante();
+
+        estudante1.nome = "Midoriya";
+        estudante1.idade = 15;
+        estudante1.sexo = 'M';
+
+        estudante2.nome = "Sakura";
+        estudante2.idade = 16;
+        estudante2.sexo = 'F';
+
+        estudante1.imprime();
+        estudante2.imprime();
+    }
+}
