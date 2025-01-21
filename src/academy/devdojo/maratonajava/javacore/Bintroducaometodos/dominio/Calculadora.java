2e24700980fe3988bf0fbad4cba286faf6ffb49d
fernandojsmelo <fernandojsmelo@gmail.com>
2025-01-21 09:31:05 -0300

    49 - Orientação Objetos - Métodos pt 06 - Parâmetros tipo referência

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -43,4 +43,13 @@ public class Calculadora {
         }
         System.out.println(num1/num2);
     }
+
+    public void alteraDoisNumeros(int num1, int num2){
+        num1 = 99;
+        num2 = 33;
+
+        System.out.println("Dentro do AlteraDoisNumeros");
+        System.out.println("num1 "+ num1);
+        System.out.println("num2 "+ num2);
+    }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante1 = new Estudante();
+        Estudante estudante2 = new Estudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
+        estudante1.nome = "Midoriya";
+        estudante1.idade = 15;
+        estudante1.sexo = 'M';
+
+        estudante2.nome = "Sakura";
+        estudante2.idade = 16;
+        estudante2.sexo = 'F';
+
+        impressora.imprime(estudante1);
+        impressora.imprime(estudante2);
+
+    }
+}
