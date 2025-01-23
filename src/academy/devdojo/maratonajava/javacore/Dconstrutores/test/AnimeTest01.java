043d23638749c6dfd6e0c101f8523ef79a61cc74
fernandojsmelo <fernandojsmelo@gmail.com>
2025-01-23 09:43:39 -0300

    59 - Orientação Objetos - Construtores pt 02 - Sobrecarga

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,11 +4,8 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Hikyuu", "TV", 12,"Ação");
-
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Hikyuu", "TV", 12,"Ação", "Production IG");
 
         anime.impreme();
-        anime2.impreme();
     }
 }
