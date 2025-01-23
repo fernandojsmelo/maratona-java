516f1a7d6b55c7b8abf3960a9dd6fe76b9b93973
fernandojsmelo <fernandojsmelo@gmail.com>
2025-01-23 15:21:34 -0300

    62 - Orientação Objetos - Métodos estáticos

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -9,7 +9,7 @@ public class CarroTest01 {
         Carro carro2 = new Carro("Mercedes", 275);
         Carro carro3 = new Carro("Audi", 290);
 
-        Carro.velocidadeLimite = 180;
+        Carro.setVelocidadeLimite(180);
 
         carro1.imprime();
         carro2.imprime();
