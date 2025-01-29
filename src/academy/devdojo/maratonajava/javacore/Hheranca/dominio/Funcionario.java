eef43ce3483ad16c66231c2eb5394de36727214a
fernandojsmelo <fernandojsmelo@gmail.com>
2025-01-29 15:39:58 -0300

    73 - Orientação Objetos - Herança pt 03 - protected

--- src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -1,12 +1,16 @@
 package academy.devdojo.maratonajava.javacore.Hheranca.dominio;
 
-public class Funcionario extends Pessoa{
+public class Funcionario extends Pessoa {
     private double salario;
 
     @Override
     public void imprime() {
         super.imprime();
         System.out.println(this.salario);
+    }
+
+    public void relatorioPagamento() {
+        System.out.println("Eu " + this.nome + " recebi o salário de " + this.salario);
     }
 
     public double getSalario() {
---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/Funcionario2.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class Funcionario2 extends Pessoa {
+    public void imprime(){
+        this.nome = "sasa";
+    }
+}
