4c22c8040a156df77acc8a10d42df7650763aa1e
fernandojsmelo <fernandojsmelo@gmail.com>
2025-01-30 10:38:48 -0300

    79 - Orientação Objetos - Modificador final pt 03 - Classes e métodos

--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -1,12 +1,17 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
 
 public class Carro {
-    private String nome;
-    public static final double VELOCIDADE_LIMITE ;
-    public final Comprador COMPRADOR = new Comprador();
+    public static final double VELOCIDADE_LIMITE;
 
     static {
         VELOCIDADE_LIMITE = 250;
+    }
+
+    public final Comprador COMPRADOR = new Comprador();
+    private String nome;
+
+    public final void imprime() {
+        System.out.println(this.nome);
     }
 
     public String getNome() {
