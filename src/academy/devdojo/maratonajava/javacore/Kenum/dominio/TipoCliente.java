46b6249b789c63c9d4f104d280ca089388457380
fernandojsmelo <fernandojsmelo@gmail.com>
2025-01-30 14:38:31 -0300

    81 - Orientação Objetos - Enumeração pt 02 - Construtores e atributos

--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -1,6 +1,22 @@
 package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+
+    private final int VALOR;
+    private String nomeRelatorio;
+
+    TipoCliente(int valor, String nomeRelatorio){
+        this.VALOR = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
+
+    public int getVALOR() {
+        return VALOR;
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -3,16 +3,15 @@ package academy.devdojo.maratonajava.javacore.Kenum.test;
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
+import static academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente.TipoPagamento;
+
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Tsubasa",TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Tsubasa",TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Tsubasa",TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira",TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
+
     }
 }
