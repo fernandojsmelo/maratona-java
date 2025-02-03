56ed7df39663381816d58d04961cf6bdfc67df67
fernandojsmelo <fernandojsmelo@gmail.com>
2025-02-03 10:42:42 -0300

    89 - Orientação Objetos - Interfaces pt 03 - Atributos e métodos estáticos

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
 public class DatabaseLoader implements DataLoader, DataRemover{
+    // private -> default -> protected -> public
     @Override
     public void Load() {
         System.out.println("Carregando dados do banco de dados");
@@ -14,5 +15,9 @@ public class DatabaseLoader implements DataLoader, DataRemover{
     @Override
     public void checkPermission() {
         System.out.println("Checando permissões no banco de dados");
+    }
+
+    public static void retieveMaxDataSize(){
+        System.out.println("Dentro do retieveMaxDataSize na classe DatabaseLoader");
     }
 }
