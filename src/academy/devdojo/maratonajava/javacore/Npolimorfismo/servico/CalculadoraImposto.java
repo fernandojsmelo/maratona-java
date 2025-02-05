95daa66dbfce3f56eb67b50ec0f82d2533a11e65
fernandojsmelo <fernandojsmelo@gmail.com>
2025-02-05 10:45:51 -0300

    93 - Orientação Objetos - Polimorfismo pt 04 - Cast e instanceof

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.servico;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
 //    public static void calcularImpostoComputador(Computador computador) {
@@ -27,5 +28,14 @@ public class CalculadoraImposto {
         System.out.println("Produto : " + produto.getNome());
         System.out.println("Preço : " + produto.getValor());
         System.out.println("Imposto a ser pago : " + imposto);
+
+        if (produto instanceof Tomate) {
+            Tomate tomate = (Tomate) produto;
+            System.out.println(tomate.getDataValidade());
+
+            String dataValidade = ((Tomate) produto).getDataValidade();
+            System.out.println(dataValidade);
+
+        }
     }
 }
