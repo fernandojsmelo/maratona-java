3fa2bc8393f4147735138d6429bcfc908f000fa4
fernandojsmelo <fernandojsmelo@gmail.com>
2025-04-10 10:30:58 -0300

    167 - Coleções pt 07 - List pt 02

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -7,8 +7,19 @@ public class ListTest01 {
     public static void main(String[] args) {
         //List nomes= new ArrayList(); // versão 1.4
         List<String> nomes= new ArrayList<>(16); // versão 1.5
+        List<String> nomes2= new ArrayList<>(16); // versão 1.5
         nomes.add("William");
-        nomes.add("DevDojo Acdemy");
+        nomes.add("DevDojo");
+
+        nomes2.add("Suane");
+        nomes2.add("Acdemy");
+
+        nomes.addAll(nomes2);
+
+        //nomes.remove(0); // ussando um index
+        nomes.remove("william"); // ussando um objeto
+        System.out.println(nomes.remove("william")); // ussando um objeto
+
 
         for (String nome: nomes){
             System.out.println(nome);
