94a192cd3093842621769404af7d9d4872fa5db4
fernandojsmelo <fernandojsmelo@gmail.com>
2025-04-10 10:17:04 -0300

    166 - Coleções pt 06 - List pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest01 {
+    public static void main(String[] args) {
+        //List nomes= new ArrayList(); // versão 1.4
+        List<String> nomes= new ArrayList<>(16); // versão 1.5
+        nomes.add("William");
+        nomes.add("DevDojo Acdemy");
+
+        for (String nome: nomes){
+            System.out.println(nome);
+        }
+
+        System.out.println("-----------------------------------");
+
+        nomes.add("Suane");
+        for (int i = 0; i < nomes.size(); i++) {
+            System.out.println(nomes.get(i));
+        }
+    }
+}
