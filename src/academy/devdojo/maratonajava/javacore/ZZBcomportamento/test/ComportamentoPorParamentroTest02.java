9361fb70ec8f2fe2d69282a306d65f7588c2ab30
fernandojsmelo <fernandojsmelo@gmail.com>
2025-09-12 09:36:01 -0300

    194 - Parametrizando comportamentos pt 02

---
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/test/ComportamentoPorParamentroTest02.java
@@ -0,0 +1,43 @@
+package academy.devdojo.maratonajava.javacore.ZZBcomportamento.test;
+
+import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Car;
+import academy.devdojo.maratonajava.javacore.ZZBcomportamento.interfaces.CarPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ComportamentoPorParamentroTest02 {
+    private static List<Car> cars = List.of(
+                    new Car("green", 2011),
+                    new Car("black", 1998),
+                    new Car("red", 2019)
+    );
+
+    static void main(String[] args) {
+//        List<Car> greenCars = filter(cars, new CarPredicate() {
+//            @Override
+//            public boolean test(Car car) {
+//                return car.getColor().equals("green");
+//            }
+//        });
+
+        List<Car> greenCars = filter(cars, car -> car.getColor().equals("green"));
+        List<Car> redCars = filter(cars, car -> car.getColor().equals("red"));
+        List<Car> yearBeforeCars = filter(cars, car -> car.getYear() < 2015);
+
+        System.out.println(greenCars);
+        System.out.println(redCars);
+        System.out.println(yearBeforeCars);
+
+    }
+    private static List<Car> filter(List<Car> cars, Predicate<Car> carPredicate) {
+        List<Car> filteredCar = new ArrayList<>();
+        for (Car car : cars) {
+            if(carPredicate.test(car)){
+                filteredCar.add(car);
+            }
+        }
+        return filteredCar;
+    }
+}
