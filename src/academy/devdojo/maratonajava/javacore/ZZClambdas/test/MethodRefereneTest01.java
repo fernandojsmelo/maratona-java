a6c06ce26c9230804d7d443486578de243a070e7
fernandojsmelo <fernandojsmelo@gmail.com>
2026-03-10 09:21:28 -0300

    198 - Method Reference pt 01 - Referência a métodos estáticos

---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodRefereneTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MethodRefereneTest01 {
+    static void main() {
+        List<Anime> animeList = new ArrayList<>(List.of(
+                new Anime("Berserk",43),
+                new Anime("One Piece",900),
+                new Anime("Naruto",500)
+        ));
+        //Collections.sort(animeList, (a1, a2) ->
+        //        a1.getTitle().compareTo(a2.getTitle()));
+        //animeList.sort( AnimeComparators::compareByTitle);
+        animeList.sort( AnimeComparators::compareByEpisodes);
+        System.out.println(animeList);
+    }
+
+}
