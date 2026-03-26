47b0300cd573ee647bfbbeb4db135eac86b51deb
fernandojsmelo <fernandojsmelo@gmail.com>
2026-03-26 11:19:05 -0300

    243 - Concorrência pt 15 - CompletableFuture pt 04 - Encadeando chamadas pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/CompletableFutureTest04.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import academy.devdojo.maratonajava.javacore.ZZGconcorrencia.dominio.Quote;
+import academy.devdojo.maratonajava.javacore.ZZGconcorrencia.service.StoreServiceWithDiscount;
+
+import java.util.List;
+
+public class CompletableFutureTest04 {
+    static void main(String[] args) {
+        StoreServiceWithDiscount service = new StoreServiceWithDiscount();
+        searchPricesWithDiscount(service);
+    }
+
+    private static void searchPricesWithDiscount(StoreServiceWithDiscount service){
+        List<String> stores = List.of("store 1", "store 2", "store 3", "store 4");
+//        stores.forEach(s -> System.out.println(service.getPriceSync(s)));
+        stores.stream()
+                .map(service::getPriceSync)
+                .map(Quote::newQuote)
+                .map(service::applyDiscount)
+                .forEach(System.out::println);
+    }
+}
