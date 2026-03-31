441d150429d82670573826b0faf06574d6c7b0af
fernandojsmelo <fernandojsmelo@gmail.com>
2026-03-31 17:09:24 -0300

    255 - JDBC pt 04 - Instalando e adicionando maven ao projeto-2

---
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/conn/ConnectFactory.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.ZZIjdbc.conn;
+
+public class ConnectFactory {
+    // java.sql = Connection, Statement, ResultSet, DriverManager
+    public static void main(String[] args) {
+
+    }
+}
