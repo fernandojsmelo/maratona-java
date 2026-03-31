1faff0f644426aa75015e398a4cfbff193934db5
fernandojsmelo <fernandojsmelo@gmail.com>
2026-03-31 16:56:30 -0300

    255 - JDBC pt 04 - Instalando e adicionando maven ao projeto

---
+++ untitled/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $grupId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
